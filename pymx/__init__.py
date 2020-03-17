e6e82b1b5c45e52818447a085b2c8415942b44e1
dataisland <dataisland@sjtu.edu.cn>
2020-03-17 16:46:27 +0800

    add lexer

---
+++ pymx/__init__.py
@@ -0,0 +1,44 @@
+""" ACM Class 2018 Compiler Homework """
+
+import argparse
+
+from .lexer import tokenize
+
+from .utils import print_tokens
+from .errors import error_collector, CompilerError
+
+def main():
+    arguemnts = get_arguments()
+
+    objs = []
+    for file in arguemnts.files:
+        objs.append(process_file(file, arguemnts))
+
+    return error_collector.show()
+
+def get_arguments():
+    parser = argparse.ArgumentParser(
+        description='Pymx is a Mx compiler created in Python', 
+        usage='pymx [-h] [options] files...'
+    )
+
+    parser.add_argument('files', metavar='files', nargs="+")
+    parser.add_argument('-c', dest='syntax_only', action='store_true')
+    return parser.parse_args()
+
+def process_file(file, args):
+    code = read_file(file)
+
+    token_list = tokenize(code, file)
+    if not error_collector.ok():
+        return None
+    
+    print_tokens(token_list)
+
+def read_file(file):
+    try:
+        with open(file) as mx_file:
+            return mx_file.read()
+    except IOError:
+        error_collector.add(CompilerError(f'Can not open {file}'))
+    return ''
---
+++ pymx/utils.py
@@ -0,0 +1,16 @@
+def log(file):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            with open(file, 'w') as f:
+                f.write(func(*args, **kw))
+        return wrapper
+    return decorator
+
+@log('lexer.log')
+def print_tokens(tokens):
+    token_info = '{:<15} {}\n'.format('Type', 'Content')
+    token_info += '------------------------------\n'
+    for token in tokens:
+        token_info += '{:<15} {}\n'.format(token.text, token.kind)
+    token_info += '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
+    return token_info
