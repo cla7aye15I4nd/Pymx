2694f758f5e6e96a3e3122eb598e7e0e4c789d49
dataisland <dataisland@sjtu.edu.cn>
2020-04-08 00:21:33 +0800

    Only one ret

---
+++ pymx/IRbuilder/SSAbuilder/optimizer/fmt.py
@@ -0,0 +1,22 @@
+from pymx.fakecode import Label
+from pymx.fakecode.inst import Ret, Phi, Jump
+from ..cfg import Block
+
+def optimize(cfg):
+    ret_stmt = []
+    for block in cfg.block.values():
+        ret = block.code[-1]
+        if type(ret) is Ret and ret.reg:
+            ret_stmt.append((ret.reg, Label(block.label)))
+    
+    if len(ret_stmt) > 1:        
+        block = Block(max(cfg.order) + 1)
+
+        reg = cfg.get_var(ret_stmt[0][0])
+        block.add_inst(Phi(reg, ret_stmt))
+        block.add_inst(Ret(reg))
+        cfg.add_block(block)
+
+        for _, blk in ret_stmt:
+            cfg.block[blk.label].code[-1] = Jump(Label(block.label))
+            
